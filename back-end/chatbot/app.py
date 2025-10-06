6a939f160459c65da458e5fbfb01d464366b0aa6
fahri22002 <fahrinizar23@gmail.com>
2025-10-06 13:30:52 +0700

    just testing

--- back-end/chatbot/app.py
+++ back-end/chatbot/app.py
@@ -1,22 +1,25 @@
-import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-from fastapi.middleware.cors import CORSMiddleware
+# Variabel global
+temp = 0
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # atau ["http://localhost:5000"]
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    global temp
+    temp += 1
+    return {"Hello": "World", "Temp": temp}
 
 if __name__ == "__main__":
+    import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8080)
